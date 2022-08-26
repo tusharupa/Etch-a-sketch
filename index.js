2bc101dd68ba82ba0d0ea5a15222e3866972c477
tushar upadhyay <tusharupa99@gmail.com>
2022-08-27 01:02:19 +0530

    basic model done

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-const container = document.querySelector('#container');
-let userInput = document.getElementById('size');
-const reset = document.querySelector('.buttons.reset')
-createGrid = () =>{
-for(let i=0;i<256;i++)
-{
-    let divs = document.createElement('div');
-    divs.className ='grid';
-    container.appendChild(divs);
-}
-}
+const container=document.querySelector('#container');
+const inputsize = document.querySelector('#input');
+const reset =document.querySelector('#reset');
 
-updateGrid = () =>{
-    container.innerText="";
-    container.style.setProperty(
-        'grid-template-columns',`repeat(${userInput.value},1fr)`
-    );
-    container.style.setProperty('grid-template-rows',`repeat(${userInput.value},1fr)`);
-    for(let i=0;i<userInput.value*userInput.value;i++)
+const defaultGrid = () =>{
+    for(let i=0;i<256;i++)
     {
         let divs=document.createElement('div');
-        divs.className = 'grid';
-        container.appendChild(divs);
+        divs.classList.add('cell');
+        container.append(divs);
     }
-    console.log(userInput.value);
-};
-userInput.addEventListener('change',updateGrid);
-reset.addEventListener('click', function(){
+    }
+const createGrid = () =>{
+    for(let i=0;i<inputsize.value * inputsize.value;i++)
+    {
+        let divs=document.createElement('div');
+        divs.classList.add('cell');
+        container.append(divs);
+    }
+    }
+const updateGrid = () =>{
     container.innerHTML="";
-    userInput.value="";
+    container.style.setProperty('grid-template-columns',`repeat(${inputsize.value},1fr)`);
+    container.style.setProperty('grid-template-rows',`repeat(${inputsize.value},1fr)`);
+createGrid();
+
+}
+const resetGrid = () =>{
+    container.innerHTML="";
+    inputsize.value="";
     container.style.setProperty('grid-template-columns',`repeat(16,1fr)`);
     container.style.setProperty('grid-template-rows',`repeat(16,1fr)`);
-    createGrid();
-});
+    defaultGrid();
+}
+defaultGrid();
+inputsize.addEventListener('change',updateGrid);
+reset.addEventListener('click',resetGrid);
 
-createGrid();
+
+
+
+
+
